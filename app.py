04389a260ac6cf37bd426f842be0710a2eb292e6
jalnyn <alan.yuan.jly@gmail.com>
2023-09-09 01:27:39 -0400

    banaa

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Potassium("my_app")
 @app.init
 def init():
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    model = pipeline('fill-mask', model='TheBloke/llama2_70b_chat_uncensored-GPTQ', device=device)
    
     context = {
         "model": model
--- download.py
+++ download.py
@@ -5,7 +5,7 @@ from transformers import pipeline
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+    pipeline('fill-mask', model='TheBloke/llama2_70b_chat_uncensored-GPTQ')
 
 if __name__ == "__main__":
     download_model()
